6f1f7b2c98c4d899f130e2c9843a224f3c852a35
falschparker82 <dominik@raute.me>
2017-02-02 17:00:23 +0100

    update again

--- gopass.rb
+++ gopass.rb
@@ -19,8 +19,8 @@ class Gopass < Formula
 
     # system bin/"gopass completion bash > bash_completion.bash"
     # system bin/"gopass completion zsh > zsh_completion.zsh"
-    bash_completion.install "bash_completion.bash"
-    zsh_completion.install "zsh_completion.zsh"
+    bash_completion.install src/"bash_completion.bash"
+    zsh_completion.install src/"zsh_completion.zsh"
   end
 
   def caveats; <<-EOS.undent
